c5565c3f4185d1428b870e3404086e20ef682e04
Jayson Gazeley <89006649+jgazeley@users.noreply.github.com>
2021-08-27 11:32:37 -0700

    Update pcon.py

--- pcon.py
+++ pcon.py
@@ -1,37 +1,23 @@
-import time
 import RPi.GPIO as io
-io.setmode(io.BCM)
-io.setwarnings(False)
+import time
 
 psupply = 26
 relay = 16
 
+io.setwarnings(False)
+io.setmode(io.BCM)
+io.setup(relay, io.OUT)
 io.setup(psupply, io.IN, io.PUD_DOWN)
+
 pc_on = io.input(psupply)
 
-if pc_on:
-    print("PC is already ON")
-    exit()
-else:
-    print("PC is OFF")
-    print("Turn on now?")
-    key = input('y/n?: ')
-    if key == 'y':
-        io.setup(relay, io.OUT)
-        io.output(relay, 1)
-        time.sleep(.1)
-        io.output(relay, 0)
-        time.sleep(2)
-        pc_on = io.input(psupply)
-        if pc_on:
-            print("PC is ON!")
-            io.cleanup()
-            exit()
-        else:
-            print("Error: No voltage detected...")
-            io.cleanup()
-            exit()
-    if key == 'n':
-        exit()
+def power_button():
+    io.output(relay, 1)
+    time.sleep(.1)
+    io.output(relay, 0)
+
+def voltage_check():
+    if io.input(psupply) == 1:
+        print("PC is ON!")
     else:
-        print('You must enter "y" or "n"')
+        print("Error: no voltage detected...")
